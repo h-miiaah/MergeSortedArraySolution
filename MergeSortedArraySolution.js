4faa63c9d2e55e4bba69a5eea539bb3deb51ad05
h-miiaah <haim.miiaah5@gmail.com>
2022-01-10 20:52:57 -0500

    else conditional

--- MergeSortedArraySolution.js
+++ MergeSortedArraySolution.js
@@ -10,7 +10,7 @@
         if (nums1[m - 1] > nums2[n - 1]) {
             nums1[m + n - 1] = nums1[--m];
           } else {
-            
+            nums1[m + n - 1] = nums2[--n];
           }
       }
 };
